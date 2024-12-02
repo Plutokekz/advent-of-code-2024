de7fa91c41e4405ea46e59d2a1aa483c9fccd35e
plutokekz <Lukas.Mahr@campus.lmu.de>
2024-12-02 18:23:15 +0100

    Part two of day two

--- 2/main.hs
+++ 2/main.hs
@@ -1,53 +1,59 @@
 import System.IO
 import Data.List
+import Debug.Trace
 
-main = do  
+
+debug = flip trace
+
+main = do
         handle <- openFile "input.txt" ReadMode
         contents <- hGetContents handle
         let list = map (map read . words) (lines contents) :: [[Int]]
-        print list
-        print $ checkList list 0 check'
-        print $ check' [7,6,4,2,1] (>)
-        print $ check' [1,2,7,8,9] (<)
-        print $ check' [9,7,6,2,1] (>)
-        print $ check' [1,3,2,4,5] (<)
-        print $ check' [8,6,4,4,1] (>)
-        print $ check' [1,3,6,7,9] (<)
+        let extendedList = produce list
+        putStrLn "Part One:"
+        print $ checkList list 0
+        putStrLn "Part Two:"
+        print $ checkList' extendedList 0
         hClose handle
 
-checkList :: [[Int]] -> Int -> ([Int] -> (Int -> Int -> Bool) -> Bool) -> Int
-checkList [] acc _ = acc
-checkList (x:xs) acc f = do
+
+produce :: [[Int]] -> [[[Int]]]
+produce = map (\ x -> x : rm x)
+
+checkList :: [[Int]] -> Int -> Int
+checkList [] acc = acc
+checkList (x:xs) acc = do
     let cmp = incrdecr x
-    let nexacc = (if f x cmp then 1 else 0 ) + acc
-    checkList xs nexacc f
+    let nexacc = (if check x cmp then 1 else 0 ) + acc
+    checkList xs nexacc
+
+checkList' :: [[[Int]]] -> Int -> Int
+checkList' [] acc = acc
+checkList' (x:xs) acc = do
+    let valid
+            | multiCheck x = 1
+            | otherwise = 0
+    checkList' xs (acc + valid)
+
+
+rm :: [Int] -> [[Int]]
+rm [] = []
+rm (x:xs) = xs : map (x:) (rm xs)
+
+multiCheck :: [[Int]] -> Bool
+multiCheck [] = False
+multiCheck (x:xs) = do
+    let cmp = incrdecr x
+    check x cmp || multiCheck xs
 
 
 incrdecr :: [Int] -> (Int -> Int -> Bool)
 incrdecr (x:y:ys) | x > y = (>)
                   | x < y = (<)
-                  | x == y = incrdecr (y:ys)
+                  | x == y = incrdecr (x:ys)
 
 check :: [Int] -> (Int -> Int -> Bool) -> Bool
 check [_] _ = True
 check (x:y:xs) cmp = do
     let diff = abs $ x - y
-    not (diff < 1 || diff > 3) && cmp x y && check (y:xs) cmp 
-
-
-check' :: [Int] -> (Int -> Int -> Bool) -> Bool
-check' [_] _ = True
-check' (x:y:z:xs) cmp = do
-    let diff1 = abs $ x - y
-    let diff2 = abs $ x - z
-    
-    if (diff1 < 1 || diff1 > 3) || not (diff2 < 1 || diff2 > 3) then
-        if (diff2 < 1 || diff2 > 3) then
-            False
-        else
-            cmp x z && check' (z:xs) cmp
-    else
-        cmp x y && check' (y:z:xs) cmp
-check' (x:y:xs) cmp = do
-    let diff = abs $ x - y
-    not (diff < 1 || diff > 3) && cmp x y && check' (y:xs) cmp 
+    not (diff < 1 || diff > 3) && cmp x y && check (y:xs) cmp
